26a1fdc26308878fbea2866ac17ac6d43bf5962c
ark_65 <345549325@qq.com>
2018-04-01 01:01:27 +0800

    提交所有代码

---
+++ src/layouts/Head.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <div class="col-sm-2 ml-md-auto">
+      <img src="../assets/logo.png"/>
+    </div>
+    <div class="col-md-9 ml-md-auto">
+      <h1>欢迎使用北京农合金融系统</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head'
+}
+</script>
+
+<style scoped>
+  img{
+    width: 80%;}
+  h1{
+    line-height: 120px;
+  }
+  .row{
+    border-bottom: 1px solid #000;
+  }
+</style>
